6fd90711a2bab532df4e34fb9ff96e927919207a
Benoit Suttor <bsuttor@imio.be>
2020-03-10 16:33:28 +0100

    feat(captcha): add plone.formwidget.recaptcha dependency #BIBLI-7

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,7 +5,8 @@ Changelog
 1.0a7 (unreleased)
 ------------------
 
-- Nothing changed yet.
+- Add plone.formwidget.recaptcha dependency.
+  [bsuttor]
 
 
 1.0a6 (2019-01-07)
--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
 
 
 setup(
-    name='library.policy',
-    version='1.0a7.dev0',
+    name="library.policy",
+    version="1.0a7.dev0",
     description="Policy for the installation of buildout.library",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -28,46 +30,47 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='Python Plone',
-    author='Nicolas Demonte',
-    author_email='support@imio.be',
-    url='https://pypi.python.org/pypi/library.policy',
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['library'],
-    package_dir={'': 'src'},
+    keywords="Python Plone",
+    author="Nicolas Demonte",
+    author_email="support@imio.be",
+    url="https://pypi.python.org/pypi/library.policy",
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["library"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
-        'plone.api>=1.8.3',
-        'Products.GenericSetup>=1.8.2',
-        'setuptools',
-        'z3c.jbot',
-        'eea.facetednavigation',
-        'collective.faceted.taxonomywidget',
-        'collective.taxonomy',
-        'plone.app.imagecropping',
-        'plone.app.mosaic',
-        'library.core',
-        'library.theme',
-        'collective.behavior.banner',
-        'collective.behavior.gallery',
-        'collective.easyform',
-        'collective.preventactions',
-        'collective.z3cform.select2',
-        'collective.cookiecuttr',
+        "plone.api>=1.8.3",
+        "Products.GenericSetup>=1.8.2",
+        "setuptools",
+        "z3c.jbot",
+        "eea.facetednavigation",
+        "collective.faceted.taxonomywidget",
+        "collective.taxonomy",
+        "plone.app.imagecropping",
+        "plone.app.mosaic",
+        "library.core",
+        "library.theme",
+        "collective.behavior.banner",
+        "collective.behavior.gallery",
+        "collective.easyform",
+        "collective.preventactions",
+        "collective.z3cform.select2",
+        "collective.cookiecuttr",
+        "plone.formwidget.recaptcha",
     ],
     extras_require={
-        'test': [
-            'plone.app.testing',
+        "test": [
+            "plone.app.testing",
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
-        ],
+            "plone.testing>=5.0.0",
+            "plone.app.contenttypes",
+            "plone.app.robotframework[debug]",
+        ]
     },
     entry_points="""
     [z3c.autoinclude.plugin]
