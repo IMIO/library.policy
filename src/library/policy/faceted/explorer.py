1e5b199eeb6adb68b28df6fe9ca08df96d1a82ce
Christophe Boulanger <christophe.boulanger@imio.be>
2020-08-04 11:23:08 +0200

    Register custom faceted template. Map is directly loading in this template instead of in a viewlet. (For 2 columns items when hide map)

---
+++ src/library/policy/faceted/explorer.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from collective.faceted.map import _
+from collective.faceted.map.browser.map import MapView
+from plone import api
+from zope.i18n import translate
+
+import json
+
+
+class ExplorerView(MapView):
+    @property
+    def map_configuration(self):
+        map_layers = api.portal.get_registry_record("geolocation.map_layers") or []
+        config = {
+            "fullscreencontrol": api.portal.get_registry_record(
+                "geolocation.fullscreen_control", default=True
+            ),
+            "locatecontrol": api.portal.get_registry_record(
+                "geolocation.locate_control", default=True
+            ),
+            "zoomcontrol": api.portal.get_registry_record(
+                "geolocation.zoom_control", default=True
+            ),
+            "minimap": api.portal.get_registry_record(
+                "geolocation.show_minimap", default=True
+            ),
+            "addmarker": api.portal.get_registry_record(
+                "geolocation.show_add_marker", default=False
+            ),
+            "geosearch": api.portal.get_registry_record(
+                "geolocation.show_geosearch", default=False
+            ),
+            "geosearch_provider": api.portal.get_registry_record(
+                "geolocation.geosearch_provider", default=[]
+            ),
+            "default_map_layer": api.portal.get_registry_record(
+                "geolocation.default_map_layer", default=[]
+            ),
+            "map_layers": [
+                {"title": translate(_(l), context=self.request), "id": l}
+                for l in map_layers
+            ],
+            "useCluster": False,
+        }
+        return json.dumps(config)
