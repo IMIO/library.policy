34891a5acc4f6f6b2780609aba51bfbb27c67e21
thomlamb <lambertthomas10@gmail.com>
2024-12-11 09:55:14 +0100

    Delete library.theme for plone6

--- setup.py
+++ setup.py
@@ -58,7 +58,6 @@ setup(
         "plone.app.imagecropping",
         "plone.app.mosaic",
         "library.core",
-        "library.theme",
         "collective.behavior.banner",
         "collective.behavior.gallery",
         "collective.easyform",
